e0e8206230bec04b955eaeaba75668080953632d
HoangDinh <dhoangk07@gmail.com>
2018-09-27 12:03:15 +0700

    Fix grain exercise

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,22 +1,24 @@
 class Grains
   result = 0
   def self.square(number)
-    if number == 1
+    if 0 >= number || number > 64
+      raise ArgumentError.new("Please input suitable number")
+    elsif number == 1
       result = 1
     elsif number == 2
       result = 2
     elsif number == 3
       result = 4
     else
-      result = 2**number
+      result = 2 ** (number-1)
     end
     result
   end
-  def self.total(number)
-    @ranges = (1..number).to_a
+  def self.total
+    @ranges = (0..63).to_a
     arr = []
     @ranges.each do |element|
-      arr << element ** 2
+      arr << 2 ** element
     end
     arr.sum
   end

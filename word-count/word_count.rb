94cc1cd7769e1c253238cbb95f5519a903b86e76
HoangDinh <dhoangk07@gmail.com>
2018-09-27 16:00:54 +0700

    Write function for word_count exercise

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,5 +1,15 @@
 class Phrase
-  def initialize(phrase)
-
+  attr_accessor :words
+  def initialize(words)
+    @words = words
   end
+
+  def word_count
+    word_list = Hash.new(0)
+    # arrays = words.tr('\x00-\x7F','').split(" ")
+    words.downcase.scan(/\w+/).each do |word|
+      word_list[word] += 1
+    end
+    word_list
+  end 
 end

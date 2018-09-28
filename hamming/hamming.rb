143cbb5ad2eabe43e80311de0e53228e3dffe644
HoangDinh <dhoangk07@gmail.com>
2018-09-28 12:54:15 +0700

    Wip

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,5 +1,16 @@
 class Hamming
-  def self.compute(att1, att2)
-    att1 = 
+  def self.compute(att1="", att2="")
+    a = att1.split('')
+    b = att2.split('')
+    diff = (a + b) - (a & b)
+    if att1== "" && att2 == ""
+      return 0
+    elsif att1.length != att2.length 
+      raise ArgumentError.new("length not match")
+    elsif att1 === att2
+      return 0
+    else
+      return diff.length
+    end
   end
 end

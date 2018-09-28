9ce2a54a6d17ae3d933167fbfbc2a244140138e0
HoangDinh <dhoangk07@gmail.com>
2018-09-28 13:17:19 +0700

    Refactor code by as following:  1. Remove the commented code.  2. Combine two loops by using one
    https://github.com/dhoangk07/exercism-ruby/pull/4

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -6,16 +6,12 @@ class Phrase
 
   def word_count
     word_list = Hash.new(0)
-    # arrays = words.tr('\x00-\x7F','').split(" ")
-    # words.downcase.scan(/\w+('\w+)?/).each do |word|
     arrays = words.downcase.scan(/(\w+('\w+)?)/)
     arr_split = []
     arrays.each do |element|
-      arr_split << element[0]
-    end
-    arr_split.each do |res_element|
-      word_list[res_element] += 1
+      word_list[element[0]]+= 1
     end
     word_list
   end 
 end
+

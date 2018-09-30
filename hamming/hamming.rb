cc35aedde6ab0215e5f841dc76ff5bae53ec6e3d
HoangDinh <dhoangk07@gmail.com>
2018-10-01 06:46:39 +0700

    Fix function test for exercise and refactor code as well

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,23 +1,12 @@
 class Hamming
   def self.compute(att1 = "", att2 = "")
-    arrs = att1.split('')
-    brrs = att2.split('')
-    result = 0
-    if att1 == "" && att2 == ""
-    result = 0
-    elsif att1.length != att2.length 
-      raise ArgumentError.new("length not match")
-    elsif att1 === att2
-      result = 0
+    raise ArgumentError if att1.length != att2.length 
+    if att1 == "" && att2 == "" || att1 === att2
+      return  0
     else
-      arrs.each do |element|
-        for i in brrs do
-          if element != i
-            result += 1
-          end
-        end
-      end
+      result = att1.split('').select.each_with_index { |item, index| att2.split('')[index] != item }
     end
-    result
+    result.count
   end
 end
+

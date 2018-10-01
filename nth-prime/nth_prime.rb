efbab5e4e95a46ee4c4946bf39a2806dab962103
HoangDinh <dhoangk07@gmail.com>
2018-10-01 07:17:23 +0700

    WIP

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -10,11 +10,8 @@ class Prime
 
   def self.nth(number)
     result = []
-    if number == 0
-      raise ArgumentError.new("Please input suitable number")
-    elsif number == 1
-      return 2
-    end
+    raise ArgumentError if number == 0
+    result = 2 if number == 1
     for i in (2..1000000000) do
       if self.primes?(i) == true
         result << i

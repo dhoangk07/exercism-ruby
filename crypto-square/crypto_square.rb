cdc73bb9122b1b6424f6258c727eabea8ba75508
HoangDinh <dhoangk07@gmail.com>
2018-10-05 15:46:04 +0700

    Done.

--- crypto-square/crypto_square.rb
+++ crypto-square/crypto_square.rb
@@ -4,15 +4,38 @@ class Crypto
     @text = text
   end
 
+  def slice_array(array)
+    element2splice = _factor(@pure_string.length)
+    new_arrays = array.each_slice(element2splice).to_a
+    if new_arrays[0].length != new_arrays[-1].length
+      diff = new_arrays[0].length - new_arrays[-1].length
+      diff.times do |n|
+        new_arrays[-1] << " "
+      end 
+    end
+    new_arrays
+  end
+
   def ciphertext
-    pure_string = @text.downcase.scan(/\w+/).join
-    return pure_string if pure_string.length == 1 
-    return @text if @text == ''
-    length = pure_string.length
-    c = 8
-    r = 8
-    pure_string.split("").each_slice(8)
+    @pure_string = @text.downcase.scan(/\w+/).join
+    if @pure_string.length == 1 
+      return @pure_string 
+    elsif @text == ''
+      return @text
+    else
+      string_arrays = @pure_string.split("")
+        result = []
+      slice_array(string_arrays).transpose.each do |element|
+        result << element.join
+      end
+    result.join(" ")
+    end
+  end
+
+  def _factor(number)
+    for ie in 1..10000
+      return ie if ie*(ie) >= number
+      return ie+1 if ie*(ie+1) >number
+    end
   end
 end
-
-puts Crypto.new('If man was meant to stay on the ground, god would have given us roots.').ciphertext

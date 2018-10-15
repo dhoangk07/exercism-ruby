4b971f2961feae1f9ba447278aabc5130f711996
HoangDinh <dhoangk07@gmail.com>
2018-10-15 09:42:35 +0700

    Refactor code

--- 100.extend-exercise/convert-a-binary-number-to-decimal.rb
+++ 100.extend-exercise/convert-a-binary-number-to-decimal.rb
@@ -1,26 +1,10 @@
 require 'byebug'
 class ConvertABinaryNumberToDecimal
   def self._2dec(number)
-    return 0 if number == 0
-    switch(number)
-  end
-
-  def self.switch(number)
-    initial_arrays = number.to_s.split("")
-    arrays = []
     result = []
-    length = number.to_s.split("").length - 1
-    (0..length).to_a.each do |element|
-      arrays << 2**element
-    end
-    result << initial_arrays
-    result << arrays.reverse
-    b = []
-    result.transpose.each do |element|
-      b << element[0].to_i*element[1]
-    end
-    b.sum
+    number.to_s.split("").reverse.each_with_index { |digit, index| result << digit.to_i * (2**index) }
+    result.sum
   end
 end
 
-puts ConvertABinaryNumberToDecimal._2dec(100000000000000000)
+puts ConvertABinaryNumberToDecimal._2dec("1011001011101111000110000")

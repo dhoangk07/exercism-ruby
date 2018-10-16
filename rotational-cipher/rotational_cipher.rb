11b0159c76b29cc20c1e862c99b32b538a47b6d7
HoangDinh <dhoangk07@gmail.com>
2018-10-16 17:50:58 +0700

    Tip

--- rotational-cipher/rotational_cipher.rb
+++ rotational-cipher/rotational_cipher.rb
@@ -1,6 +1,37 @@
 require "byebug"
 class RotationalCipher
-  def self.rotate(string, number)
+  def self.rotate(string, value)
     initial_arrays = string.split('')
+    result = []
+    initial_arrays.each do |element|
+      if element == " "
+        result << " "
+      elsif element == element.upcase
+        result << switch(element.upcase, value)
+      elsif element.scan(/[0-9]/).length >= 1
+        result << element
+      else
+        result << switch(element, value)
+      end
+    end
+    result.join.upcase
   end
+
+  def self.switch(word, value)
+    lowercase_arrays = ('a'..'z').to_a
+    index = lowercase_arrays.index(word.downcase)
+    range = index + value
+    result = 0
+    if range < 26
+      result = lowercase_arrays[range]
+    elsif range > 26
+      result = lowercase_arrays[26 - range]
+    else 
+      result = lowercase_arrays[0]
+    end
+    result
+  end
+
 end
+
+puts RotationalCipher.rotate("O M G", 5)

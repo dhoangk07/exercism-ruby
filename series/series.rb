738f905d3f281a8ab3442a91c5b0f4b1cd5d3122
HoangDinh <dhoangk07@gmail.com>
2018-10-16 16:40:20 +0700

    Done

--- series/series.rb
+++ series/series.rb
@@ -1,5 +1,16 @@
 class Series
   def initialize(number)
+    @number = number
+  end
 
+  def slices(value)
+    raise ArgumentError if value > @number.length
+    length_value = @number.length - value
+    result = []
+    for index in (0..length_value)
+      result << @number.slice(index, value)
+    end
+    result
   end
 end
+

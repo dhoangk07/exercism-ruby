b11bdbb1b813993214410af0f17f139dda3d671c
HoangDinh <dhoangk07@gmail.com>
2018-10-16 10:09:17 +0700

    WIP

--- wordy/wordy.rb
+++ wordy/wordy.rb
@@ -3,11 +3,18 @@ class WordProblem
   def initialize(string)
     @string = string
   end
+
   def answer
-    result = []
     arrays = @string.gsub!('?', '').split(' ')
-    arrays.each do |element|
-            # debugger
+    remove_items = remove_element(arrays)
+    switch_items = switch_element(remove_items)
+    multiple_element(switch_items)
+  end
+
+  def remove_element(array)
+        # debugger
+    result = []
+    array.each do |element|
       if element.scan(/^\-?[0-9]+$/).size == 1
         result << element
       elsif element == 'plus'
@@ -22,29 +29,41 @@ class WordProblem
     end
     result
   end
+
+  def switch_element(array)
+    result = []
+    array.each do |element|
+        # debugger
+      if element.scan(/^\-?[0-9]+$/).size == 1
+        result << element
+      elsif element == 'plus'
+        result << "+"
+      elsif element == 'minus'
+        result << "-"
+      elsif element == 'multiplied'
+        result << "*"
+      elsif element == 'divided'
+        result << "/"
+      end
+    end
+    result
+  end
+
+  def multiple_element(array)
+    result = []
+    length = array.length - 3
+    for index in (0..length)
+      result << array.slice(0,3).sum
+    end
+    result
+  end
 end
 
 puts WordProblem.new("What is 2 multiplied by -2 multiplied by 3?").answer
 
-
-# class WordProblem
-#   def initialize(string)
-#     @string = string
-#   end
-#   def answer
-#         # debugger
-#     result = []
-#     remove = @string.gsub!('?', '').gsub!('What is', '')
-#     if remove.include?('plus')
-#       result = remove.gsub!('plus', '+')
-#     elsif remove.include?("minus")
-#       result = remove.gsub!('minus', '-')
-#     elsif remove.include?("multiplied by")
-#       result = remove.gsub!('multiplied by', '*')
-#     elsif remove.include?("divided by")
-#       result = remove.gsub!('divided by', '/')
-#     end
-#     # puts remove
-#     eval(result)
-#   end
-# end
+def method_name(array)
+  result
+  for i+3 in (0..array.length)
+    result << array.take(3)
+  end
+end

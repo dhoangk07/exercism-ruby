244f4345d4b3791107b0559992f26d093c19ddbd
HoangDinh <dhoangk07@gmail.com>
2018-10-17 15:08:07 +0700

    Refactor code

--- rotational-cipher/rotational_cipher.rb
+++ rotational-cipher/rotational_cipher.rb
@@ -4,34 +4,36 @@ class RotationalCipher
     initial_arrays = string.split('')
     result = []
     initial_arrays.each do |element|
-      if element == " "
+      if element == ' '
         result << " "
-      elsif element == element.upcase
-        result << switch(element.upcase, value)
-      elsif element.scan(/[0-9]/).length >= 1
+      elsif upcase?(element)
+        result << switch(element, value).upcase  
+      elsif lowercase?(element)
+        result << switch(element, value)  
+      elsif integer?(element)
         result << element
       else
-        result << switch(element, value)
+        result << element
       end
     end
-    result.join.upcase
+    result.join
   end
 
   def self.switch(word, value)
-    lowercase_arrays = ('a'..'z').to_a
-    index = lowercase_arrays.index(word.downcase)
-    range = index + value
-    result = 0
-    if range < 26
-      result = lowercase_arrays[range]
-    elsif range > 26
-      result = lowercase_arrays[26 - range]
-    else 
-      result = lowercase_arrays[0]
-    end
-    result
+    index = ('a'..'z').to_a.index(word.downcase)
+    range = index + value - 26
+    ('a'..'z').to_a[range]
   end
 
-end
+  def self.upcase?(word)
+    return true if ("A".."Z").include?(word) == true
+  end
 
-puts RotationalCipher.rotate("O M G", 5)
+  def self.lowercase?(word)
+    return true if ("a".."z").include?(word) == true
+  end
+
+  def self.integer?(number)
+    return true if ("0".."9").include?(number) == true
+  end
+end

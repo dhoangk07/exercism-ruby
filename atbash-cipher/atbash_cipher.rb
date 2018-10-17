2125992a34c08bfa0c74a145d3862a25bc4dec87
HoangDinh <dhoangk07@gmail.com>
2018-10-17 22:50:30 +0700

    Refactor code

--- atbash-cipher/atbash_cipher.rb
+++ atbash-cipher/atbash_cipher.rb
@@ -1,41 +1,23 @@
 require 'byebug'
 class Atbash
-  def self.reverse(array)
-    rev = []
-    rev << array.pop until array.empty?
-    rev
-  end 
-
-  def self.initial_hash
-    a = ('a'..'z').to_a
-    b = a.reverse
-    hash = Hash[a.zip b]
-  end
-
-  def self.slice_five(array)
-    new_array = array.each_slice(5).to_a
-    result = []
-    new_array.each do |element|
-      result << element.join
-    end
-    result.join(" ")
-  end
-  
   def self.encode(string)
-    arrays = string.downcase.scan(/\w+/).join("").split("")
+    initial_arrays = string.scan(/[a-zA-Z\d]/).join.downcase.split("")
     result = []
-    arrays.each do |element|
-      if element.scan(/^\-?[0-9]+$/).size == 1
-        result << element
-      else
-        result << initial_hash[element]
-      end
+    final = []
+    initial_arrays.each { |element| result << switch(element) }
+    result.each_slice(5){ |element| final << element.join }
+    final.join(" ")
+  end
+
+  def self.switch(word)
+    arrays = ("a".."z").to_a
+    index = arrays.index(word)
+    if ("0".."9").to_a.include?(word)
+      return word
+    else
+      return arrays[25-index]
     end
-    slice_five(result)
   end
 end
-
-# puts Atbash.encode('The quick brown fox jumps over the lazy dog.')
-
 
 

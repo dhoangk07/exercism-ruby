2358cfb436b649df60e6ed2ef84aa645999c2b4b
HoangDinh <dhoangk07@gmail.com>
2018-10-18 14:15:19 +0700

    No need to assign string to new variable.

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,9 +1,8 @@
 require 'byebug'
 class Matrix
   def initialize(string)
-    @string = string
     @initialize_arrays =[]
-    @string.each_line do |element|
+    string.each_line do |element|
       pure_string = element.gsub("\n",'').split(" ")
       @initialize_arrays << to_integer(pure_string)
     end

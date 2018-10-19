0a05a79d757737bdf607bea26ba69fcc26298b8c
HoangDinh <dhoangk07@gmail.com>
2018-10-19 23:54:28 +0700

    Done and refactor code

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -3,108 +3,54 @@ class RunLengthEncoding
   def self.encode(input)
     arrays = input.split("")
     length = arrays.length
-    result = {}
-    count = 1
-    key_arrays = []
-    value_arrays = []
-    semi_final = []
-    final = []
-    for index in 0..length
-      stop_for_loop = (arrays[index+1] != nil) && arrays[index] === arrays[index+1]
-      if stop_for_loop
-        count += 1
-      else
-        result[[count, index]] = arrays[index]
-        count = 1
-        count += 1 if stop_for_loop
+    final = ''
+    index = 0
+    while index < length
+      sub_len = 1
+      while (index + sub_len < length) && arrays[index] == arrays[index + sub_len] 
+        sub_len += 1
       end
+      sub_len > 1 ? final += "#{sub_len}#{arrays[index]}" : final += "#{arrays[index]}"
+      index += sub_len
     end
-    delete_nil = result.delete_if {|k,v| v.nil?}
-    delete_nil.keys.each do |element|
-      key_arrays << element[0]
-    end
-    value_arrays << delete_nil.values
-    semi_final << key_arrays
-    semi_final << value_arrays.flatten
-    semi_final.transpose.each do |element|
-      final << combine_element(element)
-    end
-    final.join
-  end
-
-  def self.combine_element(array)
-    result = ''
-      array.each do |element|
-        if element == 1 
-          result += ''
-        else
-          result += element.to_s
-        end
-      end
-    result
+    final
   end
 
   def self.decode(input)
-    arrays = input.scan(/\d+|\D+/)
-    result = []
-      arrays.each do |element|
-        if _word?(element)
-          result << element.split("")
-        elsif _integer?(element)
-          result << element
-        end
+    arrays = input.split("")
+    length = arrays.length
+    index = 0
+    number = ''
+    final = ''
+    while index < length
+      if _integer?(arrays[index])
+        number += arrays[index]
+      else
+        number == '' ? final += print_inline(1, arrays[index]) : final += print_inline(number.to_i, arrays[index]) 
+        number = ''
       end
-    flatten_arrays = result.flatten
-    flatten_arrays.unshift("1") if _word?(flatten_arrays[0])
-    length = flatten_arrays.length
-    semi_final_decode = []
-    for index in 0..length
-          debugger
-      if (flatten_arrays[index] != nil?) && _integer?(flatten_arrays[index])
-        semi_final_decode << flatten_arrays[index] + flatten_arrays[index+1]
-      elsif (flatten_arrays[index+1] != nil?) && _word?(flatten_arrays[index+1])
-        semi_final_decode << flatten_arrays[index+1]
-      end
+      index += 1
     end
-    final_decode = []
-    semi_final_decode.each do |element|
-      final_decode << print_inline(element)
-    end
-        # debugger
-    final_decode.join
-  end
-
-  def self._word?(input)
-    return false if input.nil?
-    input.split("").all? do |element| 
-      switch = element.downcase.ord
-      'a'.ord<= switch && switch <= 'z'.ord
-    end
+    final
   end
 
   def self._integer?(input)
-    return false if input.nil?
-    input.split("").all? do |element| 
-      switch = element.ord
-      '0'.ord <= switch && switch <= '9'.ord
-    end
+    '0'.ord <= input.ord && input.ord <= '9'.ord
   end
 
-  def self.print_inline(string)
-    arrays = string.scan(/\d+|\D+/)
-    if arrays.length == 1
-       arrays[0]
-    else
-       arrays[1]*arrays[0].to_i
-    end
+  def self.print_inline(number, element)
+    element * number
   end
-  
 end
 
-puts RunLengthEncoding.decode('2 hs2q q2w2 ')
-# puts RunLengthEncoding.decode('12WB12W3B24WB')
 
-# puts RunLengthEncoding._word?('2x')
+
+
+
+
+
+
+
 
 
 
